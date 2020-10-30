9a018d886f5bc6ae7cc4dac59d19ac91ed926271
Daniel Mendes <dadinel@hotmail.com>
2020-10-30 15:47:56 -0300

    Atualização
    Alteração para esperar quando o status estiver como "subindo", tempos de espera e mensagens para saber em qual passo o script está.

--- src/powershell/up-postgres-service.ps1
+++ src/powershell/up-postgres-service.ps1
@@ -1,7 +1,22 @@
 $status = Get-Service postgresql-x64-11 | %{$_.Status}
 
-while($status -eq "Stopped") {
-	Stop-Process -Name "postgres" -Force
-	Start-Service postgresql-x64-11
+Write-Output "Comecando a subir o Postgres..."
+
+if ($status -eq "StartPending") {
+	Write-Output "O servico esta subindo, aguardando atualizacao do status..."
+	Start-Sleep -s 99
 	$status = Get-Service postgresql-x64-11 | %{$_.Status}
+}
+
+while($status -eq "Stopped") {
+	Start-Sleep -s 45
+	Write-Output "Derrubando executaveis..."
+	Stop-Process -Name "postgres" -Force
+	Write-Output "Deletando arquivo de PID..."
+	Remove-Item "C:\Program Files\PostgreSQL\11\data\postmaster.pid" -Force
+	Write-Output "Subindo o servico..."
+	Start-Service postgresql-x64-11
+	Write-Output "Verificando o status do servico..."
+	$status = Get-Service postgresql-x64-11 | %{$_.Status}
+	Write-Output "Status do servico:" $status
 }
